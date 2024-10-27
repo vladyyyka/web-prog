1e0c04602f5ccb99353dc89e262300f89a3602d9
vladyyyka <korobeynikovlife@gmail.com>
2024-10-28 00:01:45 +0500

    Delete lab4 directory

--- lab4/client/src/main.js
+++
@@ -1,8 +0,0 @@
-
-import { createApp } from 'vue';
-import App from './App.vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
-createApp(App).mount('#app');
